862f48de0ee9bd031cc85431a4b1508436e3c8a8
Renzo Zahír Diaz Silva <69408733+RiH-ub@users.noreply.github.com>
2023-01-18 16:58:30 +0000

    Cambio de nombre "EEE"

--- EEE.py
+++ EEE.py
@@ -3,7 +3,8 @@ print("Hola ")
 print("Hola como estas tu")
 num1 = float(input("Introduce tu nombre: "))
 num2 = float(input("Introduce tu apellido: "))
-resul = num1 + num2 
+resul = num1 + num2 / 1.0
 print(resul)
 
 print("Hola")
+print("Resultados finales")
