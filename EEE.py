4b9476dcfba8b442fa91027c1c208647c10907e9
RiH-ub <renzozahir2001@gmail.com>
2023-01-22 15:11:46 -0500

    hola

--- EEE.py
+++ EEE.py
@@ -1,5 +1,27 @@
-num1 = int(input("numero 1: "))
-num2 = int(input("numero 2: "))
+while True:
 
-rpta = num1 + num2
-print(f"tu resultado es {rpta} jaja")
+  cat = input("Polar o Titi: ")
+  print("××××××××××××××××××")
+
+  if (cat == "polar" or cat == "Polar"): break
+    print("¡Escogiste a Polar!")
+    print("Vida: 70")
+    print("Daño: 300")
+    print("Movilidad: 200")
+    print("----------------")
+    print("Polar se encontraba caminando tranquilamente por el bosque. hasta que en un momento se encontro con Titi.")
+    print("Que es lo que haras?")
+     op1 = int(input("A Haras daño /n B Invocas a las gatas /n Mueres"))
+     op2 
+    
+
+  
+
+elif cat == "titi" or cat == "Titi":
+  print("¡Escogiste a Titi!")
+  print("Vida: 100+")
+  print("Daño: 210+")
+  print("Movilidad: 70")
+
+print("-----------------------------")
+print("cambios")
