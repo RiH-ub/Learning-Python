69ac8bc4729c936857c3d43d384f228e343e260a
RiH-ub <renzozahir2001@gmail.com>
2023-02-22 01:18:33 +0000

    hola

--- prácticas/test_1.py
+++ prácticas/test_1.py
@@ -1,9 +1,1 @@
-Algoritmo suma_producto_2_numeros
-Escribir 'Ingrese 2 números'
-Leer num1, num2
-suma & num1+num?
-multp « num1 *num2
-Escribir 'La suma es:
-',suma
-Escribir 'La multiplicación es: ',multp
-8 FinAlgoritmo
+print("hla")
