4241154aed89ab0ad99fa0673a3247a051af58bd
RiH-ub <renzozahir2001@gmail.com>
2023-03-01 03:55:15 +0000

    maka

--- Practice/A.py
+++ Practice/A.py
@@ -5,15 +5,19 @@
   # Variable de control 
 
 # Mostrar los numeros del 1 al 10
-i = 1
-
-while
-
-
+"""i = 1
 
 while i <= 100:
   if i % 2 == 0:
     print(i)
 
   i += 1
+"""
 
+# Elaborar un menu de opciones
+
+def suma(a):
+  for i in range (a,13+a):
+    print(f"{i} + {a} = {i+a}")
+
+suma(7)
