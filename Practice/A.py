522bcc7f5949ff8122d4091e7f6b33a16b2ecfd4
RiH-ub <renzozahir2001@gmail.com>
2023-03-01 01:51:47 +0000

    hola

--- Practice/A.py
+++ Practice/A.py
@@ -1,8 +1,19 @@
 # Bucles
+# Variables de inicialización (estado) 
+# while condición:
+  # codigo a repetir
+  # Variable de control 
 
+# Mostrar los numeros del 1 al 10
 i = 1
-while i <= 10:
-  print (i)
-  i += 3
 
+while
+
+
+
+while i <= 100:
+  if i % 2 == 0:
+    print(i)
+
+  i += 1
 
